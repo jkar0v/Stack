059e3a100571d4d3f7388e05dcb69b848daa69ee
MEGA <MEGA@gmail.com>
2025-03-17 17:25:19 +0200

    Add project files.

---
+++ zad 1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace zad_1
+{
+    internal class Program
+    {
+            static Stack<char> stack = new Stack<char>();
+        static void Main(string[] args)
+        {
+            stack.Push('H');
+            stack.Push('a');
+            stack.Push('l');
+            stack.Push('l');
+            stack.Push('o');
+
+            PrintStack();
+        }
+        static void PrintStack()
+        {
+            foreach (char c in stack)
+            {
+                Console.Write(c);
+            }
+        }
+    }
+}
---
+++ zad 2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace zad_2
+{
+    internal class Program
+    {
+            static Stack<char> stack = new Stack<char>();
+        static void Main(string[] args)
+        {
+
+           
+        }
+        static void Look()
+        {
+            int see = 0;
+            foreach (char c in stack)
+            {
+                if (c == ')')
+                    see++;
+                else if (c == ']')
+                    see =+ 2;
+                else if (c == '}')
+                    see = +3;
+                else if (c == '{')
+                    see = -3;
+                else if (c == '[')
+                    see = -2;
+                else if (c == '(')
+                    see--;
+            }
+        }
+    }
+}
