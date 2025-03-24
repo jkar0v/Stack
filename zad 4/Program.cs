3de610ad8ddee6f0fbd160d831714d38fab8045a
Jivko Karov <jikito2007@gmail.com>
2025-03-24 11:05:53 +0200

    Задача 4 е завършена, с това и целият Stack до момента

--- zad 4/Program.cs
+++ zad 4/Program.cs
@@ -7,12 +7,27 @@
             Console.WriteLine("Въведете число от интервала 5 до 55:");
             int n = int.Parse(Console.ReadLine());
 
-            Stack<int> stack = new Stack<int>();
+            if (n < 5 || n > 55)
+            {
+                Console.WriteLine("Числото трябва да бъде между 5 и 55" +
+                    "\nРестартирайте програмата за нов опит!");
+                return;
+            }
 
-        }
-        internal void Fibonacci(int n)
-        {
+            Stack<int> fibonacciStack = new Stack<int>();
+            int a = 1, b = 1;
 
+            fibonacciStack.Push(a);
+            fibonacciStack.Push(b);
+
+            for (int i = 3; i <= n; i++)
+            {
+                int next = a + b;
+                fibonacciStack.Push(next);
+                a = b;
+                b = next;
+            }
+            Console.WriteLine("Изход: " + string.Join("; ", fibonacciStack));
         }
     }
 }
